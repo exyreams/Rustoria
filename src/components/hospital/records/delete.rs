daa8715282cd49556143047829566e5051a0db63
Exyreams <43289621+exyreams@users.noreply.github.com>
2025-03-12 07:15:24 +0000

    fix: highlighting color

--- src/components/hospital/records/delete.rs
+++ src/components/hospital/records/delete.rs
@@ -416,7 +416,7 @@ impl Component for DeleteRecord {
 
         let selected_style = Style::default()
             .bg(Color::Rgb(45, 45, 60))
-            .fg(Color::Rgb(250, 250, 250))
+            .fg(Color::Rgb(250, 250, 110))
             .add_modifier(Modifier::BOLD);
         let normal_style = Style::default()
             .bg(Color::Rgb(26, 26, 36))
